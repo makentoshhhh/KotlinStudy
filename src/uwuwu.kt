f042e8c41bfbd5ddd3619f45f569fb03ef9da4e3
student <kirill.prikhodko@outlook.com>
2024-01-26 09:59:24 +0300

    что то пытался но и списки изучил

--- src/uwuwu.kt
+++ src/uwuwu.kt
@@ -1,10 +1,7 @@
-fun main() {
-    val z:Int = readLine()!!.toInt()
-    val y:String = readLine()!!
-    val x:Int = readLine()!!.toInt()
-calculate(z,x,y)
+//fun main() {
+    //uw()
 
-}
+
 fun calculate(num1:Int, num2: Int, operand: String){
     when(operand) {
         "+" -> {
@@ -12,8 +9,28 @@ fun calculate(num1:Int, num2: Int, operand: String){
             print("$num1 + $num2 = ${num1 + num2}")
         }
         "-" -> print("$num1 + $num2 = ${num1 - num2}")
-        "*" -> print("$num1 + $num2 = ${num1 * num2}")
-        "/" -> print("$num1 + $num2 = ${num1 / num2}")
+        "*" -> print("$num1 * $num2 = ${num1 * num2}")
+        "/" -> print("$num1 / $num2 = ${num1 / num2}")
         else -> print("Введено не коректное число")
     }
+}
+fun uw(){
+    val z:Int = readLine()!!.toInt()
+    val y:String = readLine()!!
+    val x:Int = readLine()!!.toInt()
+   calculate(z,x,y)
+}
+fun task2(){
+    val m:List <Int> = listOf(1,2,3,4,5,6,7,8)
+    val p:MutableList <Int> = mutableListOf(1,2,3,4,5)
+    p.add(6)
+    val o:Map <Int,Int> = mapOf()
+    val c:HashMap <Int,Int> = hashMapOf()
+
+}
+fun poisk(){
+    val a:MutableList <Int> = mutableListOf()
+    a.add()
+
+
 }
